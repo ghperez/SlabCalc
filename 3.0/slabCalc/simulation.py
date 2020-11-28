6936f7a4695b7deb9bad753aa0239f8754ac9ec2
gperez <gabrielhenriperez@gmail.com>
2020-11-27 22:54:21 -0300

    fixed

--- 3.0/slabCalc/simulation.py
+++ 3.0/slabCalc/simulation.py
@@ -72,7 +72,7 @@ class Sim(object):
 	def run_qe(self,cmd,silent=False,save_when_done=True,savefile="sim.pickle"):
 		"""
 		"""
-		for i in range(len(self.slabs)):
+		for i in range(len(self.dat)):
 			saveinp = self.dat[i]["saveinp"]
 			inpfile = self.dat[i]["inpfile"]
 			
