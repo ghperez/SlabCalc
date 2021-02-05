b2e0b2372ad870778739683bd4e588e744da5e4b
Gabriel <gabriel.perez@titanio-login.cluster.titanio.ufabc.edu.br>
2021-02-05 19:46:36 -0300

    removed TCNQ

--- TCNQ/s1/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -n 64                
-#SBATCH --ntasks-per-node=64 
-#SBATCH -p medium            
-#SBATCH --job-name=SlabCalc
-
-cd $SLURM_SUBMIT_DIR
-
-mpirun -np $SLURM_NTASKS pw.x < TCNQ-s1.in 1> TCNQ-s1.out 2> err
